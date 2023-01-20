3347e5b7095a42d318a3c0952823e242790b29dd
Robert Wolniewicz <robert_wol@wp.pl>
2023-01-20 21:28:48 +0100

    Pagination icons on main Window

--- ONWELO VOTE APP/MainWindow.Designer.cs
+++ ONWELO VOTE APP/MainWindow.Designer.cs
@@ -31,7 +31,7 @@
             this.components = new System.ComponentModel.Container();
             this.voterBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.candidateBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.VotersDataGridView = new System.Windows.Forms.DataGridView();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.UserLable = new System.Windows.Forms.Label();
             this.InfoLabel3 = new System.Windows.Forms.Label();
@@ -50,9 +50,19 @@
             this.VotersGroupBox = new System.Windows.Forms.GroupBox();
             this.CandidatesGroupBox = new System.Windows.Forms.GroupBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.VotersTotalResultLabel = new System.Windows.Forms.Label();
+            this.VotersResultLabel = new System.Windows.Forms.Label();
+            this.VotersPagesLabel = new System.Windows.Forms.Label();
+            this.VotersTotalPagesLabel = new System.Windows.Forms.Label();
+            this.VotersCurrentPageTextBox = new System.Windows.Forms.TextBox();
+            this.CandidatesCurrentPageTextBox = new System.Windows.Forms.TextBox();
+            this.CandidatesPagesLabel = new System.Windows.Forms.Label();
+            this.CandidatesResultsLabel = new System.Windows.Forms.Label();
+            this.CandidatesTotalResultsLabel = new System.Windows.Forms.Label();
+            this.CandidatesTotalPagesLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.voterBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.candidateBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VotersDataGridView)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.VotersGroupBox.SuspendLayout();
             this.CandidatesGroupBox.SuspendLayout();
@@ -67,14 +77,14 @@
             // 
             this.candidateBindingSource.DataSource = typeof(ONWELO_VOTE_APP.Entity.Candidate);
             // 
-            // dataGridView
+            // VotersDataGridView
             // 
-            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView.Location = new System.Drawing.Point(6, 90);
-            this.dataGridView.Name = "dataGridView";
-            this.dataGridView.RowTemplate.Height = 25;
-            this.dataGridView.Size = new System.Drawing.Size(428, 296);
-            this.dataGridView.TabIndex = 0;
+            this.VotersDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.VotersDataGridView.Location = new System.Drawing.Point(6, 90);
+            this.VotersDataGridView.Name = "VotersDataGridView";
+            this.VotersDataGridView.RowTemplate.Height = 25;
+            this.VotersDataGridView.Size = new System.Drawing.Size(358, 296);
+            this.VotersDataGridView.TabIndex = 0;
             // 
             // dataGridView1
             // 
@@ -82,7 +92,7 @@
             this.dataGridView1.Location = new System.Drawing.Point(6, 90);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(428, 296);
+            this.dataGridView1.Size = new System.Drawing.Size(364, 296);
             this.dataGridView1.TabIndex = 2;
             // 
             // UserLable
@@ -159,7 +169,7 @@
             // AddCandidateButton
             // 
             this.AddCandidateButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.AddCandidateButton.Location = new System.Drawing.Point(347, 47);
+            this.AddCandidateButton.Location = new System.Drawing.Point(288, 47);
             this.AddCandidateButton.Name = "AddCandidateButton";
             this.AddCandidateButton.Size = new System.Drawing.Size(87, 37);
             this.AddCandidateButton.TabIndex = 13;
@@ -222,8 +232,13 @@
             // 
             // VotersGroupBox
             // 
+            this.VotersGroupBox.Controls.Add(this.VotersCurrentPageTextBox);
+            this.VotersGroupBox.Controls.Add(this.VotersTotalPagesLabel);
+            this.VotersGroupBox.Controls.Add(this.VotersPagesLabel);
+            this.VotersGroupBox.Controls.Add(this.VotersResultLabel);
+            this.VotersGroupBox.Controls.Add(this.VotersTotalResultLabel);
             this.VotersGroupBox.Controls.Add(this.VotersSearchTextBox);
-            this.VotersGroupBox.Controls.Add(this.dataGridView);
+            this.VotersGroupBox.Controls.Add(this.VotersDataGridView);
             this.VotersGroupBox.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.VotersGroupBox.Location = new System.Drawing.Point(12, 54);
             this.VotersGroupBox.Name = "VotersGroupBox";
@@ -234,6 +249,11 @@
             // 
             // CandidatesGroupBox
             // 
+            this.CandidatesGroupBox.Controls.Add(this.CandidatesTotalPagesLabel);
+            this.CandidatesGroupBox.Controls.Add(this.CandidatesCurrentPageTextBox);
+            this.CandidatesGroupBox.Controls.Add(this.CandidatesPagesLabel);
+            this.CandidatesGroupBox.Controls.Add(this.CandidatesResultsLabel);
+            this.CandidatesGroupBox.Controls.Add(this.CandidatesTotalResultsLabel);
             this.CandidatesGroupBox.Controls.Add(this.dataGridView1);
             this.CandidatesGroupBox.Controls.Add(this.CandidatesSearchTextBox);
             this.CandidatesGroupBox.Controls.Add(this.AddCandidateButton);
@@ -255,6 +275,104 @@
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 22;
             this.pictureBox1.TabStop = false;
+            // 
+            // VotersTotalResultLabel
+            // 
+            this.VotersTotalResultLabel.AutoSize = true;
+            this.VotersTotalResultLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.VotersTotalResultLabel.Location = new System.Drawing.Point(370, 90);
+            this.VotersTotalResultLabel.Name = "VotersTotalResultLabel";
+            this.VotersTotalResultLabel.Size = new System.Drawing.Size(72, 15);
+            this.VotersTotalResultLabel.TabIndex = 16;
+            this.VotersTotalResultLabel.Text = "Total Results";
+            // 
+            // VotersResultLabel
+            // 
+            this.VotersResultLabel.AutoSize = true;
+            this.VotersResultLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.VotersResultLabel.Location = new System.Drawing.Point(392, 118);
+            this.VotersResultLabel.Name = "VotersResultLabel";
+            this.VotersResultLabel.Size = new System.Drawing.Size(13, 15);
+            this.VotersResultLabel.TabIndex = 17;
+            this.VotersResultLabel.Text = "0";
+            // 
+            // VotersPagesLabel
+            // 
+            this.VotersPagesLabel.AutoSize = true;
+            this.VotersPagesLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.VotersPagesLabel.Location = new System.Drawing.Point(370, 190);
+            this.VotersPagesLabel.Name = "VotersPagesLabel";
+            this.VotersPagesLabel.Size = new System.Drawing.Size(38, 15);
+            this.VotersPagesLabel.TabIndex = 18;
+            this.VotersPagesLabel.Text = "Pages";
+            // 
+            // VotersTotalPagesLabel
+            // 
+            this.VotersTotalPagesLabel.AutoSize = true;
+            this.VotersTotalPagesLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.VotersTotalPagesLabel.Location = new System.Drawing.Point(402, 211);
+            this.VotersTotalPagesLabel.Name = "VotersTotalPagesLabel";
+            this.VotersTotalPagesLabel.Size = new System.Drawing.Size(18, 15);
+            this.VotersTotalPagesLabel.TabIndex = 19;
+            this.VotersTotalPagesLabel.Text = "/1";
+            // 
+            // VotersCurrentPageTextBox
+            // 
+            this.VotersCurrentPageTextBox.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.VotersCurrentPageTextBox.Location = new System.Drawing.Point(370, 208);
+            this.VotersCurrentPageTextBox.Name = "VotersCurrentPageTextBox";
+            this.VotersCurrentPageTextBox.Size = new System.Drawing.Size(26, 23);
+            this.VotersCurrentPageTextBox.TabIndex = 20;
+            this.VotersCurrentPageTextBox.Text = "1";
+            // 
+            // CandidatesCurrentPageTextBox
+            // 
+            this.CandidatesCurrentPageTextBox.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.CandidatesCurrentPageTextBox.Location = new System.Drawing.Point(372, 208);
+            this.CandidatesCurrentPageTextBox.Name = "CandidatesCurrentPageTextBox";
+            this.CandidatesCurrentPageTextBox.Size = new System.Drawing.Size(26, 23);
+            this.CandidatesCurrentPageTextBox.TabIndex = 24;
+            this.CandidatesCurrentPageTextBox.Text = "1";
+            // 
+            // CandidatesPagesLabel
+            // 
+            this.CandidatesPagesLabel.AutoSize = true;
+            this.CandidatesPagesLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.CandidatesPagesLabel.Location = new System.Drawing.Point(372, 190);
+            this.CandidatesPagesLabel.Name = "CandidatesPagesLabel";
+            this.CandidatesPagesLabel.Size = new System.Drawing.Size(38, 15);
+            this.CandidatesPagesLabel.TabIndex = 23;
+            this.CandidatesPagesLabel.Text = "Pages";
+            // 
+            // CandidatesResultsLabel
+            // 
+            this.CandidatesResultsLabel.AutoSize = true;
+            this.CandidatesResultsLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.CandidatesResultsLabel.Location = new System.Drawing.Point(394, 118);
+            this.CandidatesResultsLabel.Name = "CandidatesResultsLabel";
+            this.CandidatesResultsLabel.Size = new System.Drawing.Size(13, 15);
+            this.CandidatesResultsLabel.TabIndex = 22;
+            this.CandidatesResultsLabel.Text = "0";
+            // 
+            // CandidatesTotalResultsLabel
+            // 
+            this.CandidatesTotalResultsLabel.AutoSize = true;
+            this.CandidatesTotalResultsLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.CandidatesTotalResultsLabel.Location = new System.Drawing.Point(372, 90);
+            this.CandidatesTotalResultsLabel.Name = "CandidatesTotalResultsLabel";
+            this.CandidatesTotalResultsLabel.Size = new System.Drawing.Size(72, 15);
+            this.CandidatesTotalResultsLabel.TabIndex = 21;
+            this.CandidatesTotalResultsLabel.Text = "Total Results";
+            // 
+            // CandidatesTotalPagesLabel
+            // 
+            this.CandidatesTotalPagesLabel.AutoSize = true;
+            this.CandidatesTotalPagesLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.CandidatesTotalPagesLabel.Location = new System.Drawing.Point(404, 211);
+            this.CandidatesTotalPagesLabel.Name = "CandidatesTotalPagesLabel";
+            this.CandidatesTotalPagesLabel.Size = new System.Drawing.Size(18, 15);
+            this.CandidatesTotalPagesLabel.TabIndex = 25;
+            this.CandidatesTotalPagesLabel.Text = "/1";
             // 
             // MainWindow
             // 
@@ -279,7 +397,7 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.voterBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.candidateBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VotersDataGridView)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.VotersGroupBox.ResumeLayout(false);
             this.VotersGroupBox.PerformLayout();
@@ -295,7 +413,7 @@
 
         private BindingSource voterBindingSource;
         private BindingSource candidateBindingSource;
-        private DataGridView dataGridView;
+        private DataGridView VotersDataGridView;
         private DataGridView dataGridView1;
         private Label UserLable;
         private Label InfoLabel3;
@@ -314,5 +432,15 @@
         private GroupBox VotersGroupBox;
         private GroupBox CandidatesGroupBox;
         private PictureBox pictureBox1;
+        private TextBox VotersCurrentPageTextBox;
+        private Label VotersTotalPagesLabel;
+        private Label VotersPagesLabel;
+        private Label VotersResultLabel;
+        private Label VotersTotalResultLabel;
+        private Label CandidatesTotalPagesLabel;
+        private TextBox CandidatesCurrentPageTextBox;
+        private Label CandidatesPagesLabel;
+        private Label CandidatesResultsLabel;
+        private Label CandidatesTotalResultsLabel;
     }
 }
